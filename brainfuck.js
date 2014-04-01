a37b135ccd0f85531bb1821640d7f370cf03c790
Lexi R <lexi@lexi.org.uk>
2014-04-02 00:32:00 +0200

    Remove headers

--- brainfuck.js
+++ brainfuck.js
@@ -6,22 +6,10 @@
         var tab = document.getElementById(id);
         var thead = tab.querySelector('thead');
         var tbody = tab.querySelector('tbody');
-        var tr, th, td;
-        // tr = document.createElement('tr');
-        // th = document.createElement('td');
-        // tr.appendChild(th);
-        // for (i = 0; i < 30; i++) {
-        //     th = document.createElement('th');
-        //     th.textContent = i + 1;
-        //     tr.appendChild(th);
-        // }
-        // thead.appendChild(tr);
+        var tr, td;
         for (i = 0; i < 3; i++) {
             tr = document.createElement('tr');
             tr.id = "row-" + i;
-            // th = document.createElement('th');
-            // th.textContent = i + 1;
-            // tr.appendChild(th);
             for (j = 0; j < 30; j++) {
                 td = document.createElement('td');
                 td.textContent = 0;
