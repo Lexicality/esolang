e663cec1bb611f299926b9d232c2bbba52f70433
Lexi R <lexi@lexi.org.uk>
2014-04-04 23:02:02 +0200

    Move table script to brainfuck.html

--- brainfuck.js
+++ brainfuck.js
@@ -114,25 +114,6 @@
     // debug
     global.tokenizeProgram = tokenizeProgram;
 
-    (function(id) {
-        var i, j;
-        var tab = document.getElementById(id);
-        var thead = tab.querySelector('thead');
-        var tbody = tab.querySelector('tbody');
-        var tr, td;
-        for (i = 0; i < 3; i++) {
-            tr = document.createElement('tr');
-            tr.id = "row-" + i;
-            for (j = 0; j < 30; j++) {
-                td = document.createElement('td');
-                td.textContent = 0;
-                td.id = "cell-" + (i * 30 + j);
-                tr.appendChild(td);
-            }
-            tbody.appendChild(tr);
-        }
-    })('rams');
-
     var classes = {
         '<': 'lt',
         '>': 'gt',
