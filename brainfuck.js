cda21a134bafb6f732526736d7d81e0f27969e6b
Lex Robinson <lex@lex.me.uk>
2016-08-22 13:12:21 +0100

    Break out the utils into a util

--- brainfuck.js
+++ brainfuck.js
@@ -1,46 +1,13 @@
 (function(global) {
     'use strict';
 
-    var on = function() {
-        this.addEventListener.apply(this, arguments);
-    };
-    var empty = function() {
-        while (this.firstChild) {
-            // FIXME: Does this .remove() on the child? We don't want orphan nodes
-            this.removeChild(this.firstChild);
-        }
-    };
+    const {
+        $,
+        $$,
+        stdout,
+        stdin,
+    } = global.utils;
 
-    function $(selector) {
-        var el = document.querySelector(selector);
-        if (!el)
-            return el;
-        el.on = on;
-        el.empty = empty;
-        return el;
-    }
-
-    function $$(selector) {
-        return document.querySelectorAll(selector);
-    }
-
-    function stdout(msg) {
-        $('#stdout').textContent += msg;
-    }
-
-    function stdin() {
-        var el = $('#stdin');
-        var value = el.value;
-        if (!value.length) {
-            return '';
-        } else if (1 == value.length) {
-            el.value = '';
-            return value;
-        }
-        var ret = value.substr(0, 1);
-        el.value = value.substr(1);
-        return ret;
-    }
     var timeOutNum = 1000,
         timeOutValue = 0,
         running = false;
