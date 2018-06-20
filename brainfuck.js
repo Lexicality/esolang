23c15e8f0eee22e828b98feacf7683416e87f2bd
Lex Robinson <lex@lex.me.uk>
2018-06-20 12:26:11 +0100

    Move the ram system into its own module

--- brainfuck.js
+++ brainfuck.js
@@ -8,74 +8,20 @@
         stdin,
     } = global.utils;
 
+    const { initRam } = global.rams;
+
+    const {
+        resetRam,
+        getRam,
+        setRam,
+        decayRam,
+        incrementPointer,
+        decrementPointer,
+    } = initRam(30000);
+
     var timeOutNum = 1000,
         timeOutValue = 0,
         running = false;
-
-    var ram = [],
-        maxRams = 30000, // FIXME The gui only shows 90
-        ramPointer = 0,
-        ramStack = new global.HighlightStack();
-
-    function touchCell(cell) {
-        ramStack.promote(cell);
-    }
-
-    function updatePointer(num) {
-        var cell;
-        // TODO: Validate num
-        cell = $('#cell-' + ramPointer);
-        if (cell)
-            cell.classList.remove('active-cell');
-        ramPointer = num;
-        $('#ramPointer').textContent = ramPointer;
-        cell = $('#cell-' + num);
-        if (cell) {
-            cell.classList.add('active-cell');
-            touchCell(cell);
-        }
-    }
-
-    function incrementPointer() {
-        var ptr = ramPointer + 1;
-        if (ptr >= maxRams)
-            ptr = 0;
-        updatePointer(ptr);
-    }
-
-    function decrementPointer() {
-        var ptr = ramPointer - 1;
-        if (ptr < 0)
-            ptr = maxRams - 1;
-        updatePointer(ptr);
-    }
-
-    function getRam() {
-        return ram[ramPointer] || 0;
-    }
-
-    function setRam(value) {
-        ram[ramPointer] = value;
-        $('#memValue').textContent = "'" + String.fromCharCode(value) + "'";
-        var cell = $('#cell-' + ramPointer);
-        if (cell) {
-            cell.textContent = value;
-            touchCell(cell);
-        }
-    }
-
-    function resetRam() {
-        ram = [];
-        updatePointer(0);
-        $('#memValue').textContent = "''";
-        ramStack.clear();
-        var nodes = $$('.ram-cell');
-        var i;
-        for (i = 0; i < nodes.length; i++) {
-            nodes[i].textContent = 0;
-        }
-    }
-    resetRam();
 
     var program = [],
         programStack = new HighlightStack(),
@@ -166,7 +112,7 @@
         if (checkForProgramEnd())
             return;
         if (!didMemOperation)
-            ramStack.decay();
+            decayRam();
         if (running)
             timeOutValue = window.setTimeout(runProgram, timeOutNum);
     }
