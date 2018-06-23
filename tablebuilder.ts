b173e63c929b88210428d2115c24db25275a8bac
Lex Robinson <lex@lex.me.uk>
2018-06-24 00:57:42 +0100

    Make tablebuilder work for things other than ram
    IE my program table kthx

--- tablebuilder.ts
+++ tablebuilder.ts
@@ -8,6 +8,7 @@ export function constructTable(
 	numRows: number,
 	numColumns: number,
 	content: string = "0",
+	type: string = "ram",
 ) {
 	var createElement, i, j, table, tbody, tr, td;
 	createElement = document.createElement.bind(document);
@@ -18,12 +19,12 @@ export function constructTable(
 	tbody = table.querySelector("tbody");
 	for (i = 0; i < numRows; i++) {
 		tr = createElement("tr");
-		tr.id = "row-" + i;
+		tr.id = `${type}-row-${i}`;
 		for (j = 0; j < numColumns; j++) {
 			td = createElement("td");
 			td.textContent = content;
-			td.id = "cell-" + (i * numColumns + j);
-			td.classList.add("ram-cell");
+			td.id = `${type}-cell-${i * numColumns + j}`;
+			td.classList.add(`${type}-cell`);
 			tr.appendChild(td);
 		}
 		tbody!.appendChild(tr);
